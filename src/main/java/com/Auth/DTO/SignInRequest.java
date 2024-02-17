ccb543f0e3594faeb4412d0e3603b63d0e1f1b87
Nadar <donat290817@gmail.com>
2024-02-18 00:34:31 +0300

    Committing local changes before switching branches

--- src/main/java/com/Auth/DTO/SignInRequest.java
+++ src/main/java/com/Auth/DTO/SignInRequest.java
@@ -1,2 +1,20 @@
-package com.Auth.DTO;public class SignInRequest {
+package com.Auth.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Schema
+public class SignInRequest {
+    @Schema(description = "Имя пользователя", example = "Nadar")
+    @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
+    @NotBlank(message = "Имя пользователя не должно быть пустым")
+    private String username;
+
+    @Schema(description = "Пароль", example = "jdfvmdfmkv")
+    @Size(min = 8, max = 255, message = "Пароль должен быть от 5 до 255 символов")
+    @NotBlank(message = "Пароль не может быть пустым")
+    private String password;
 }
--- src/main/java/com/Auth/DTO/SignUpRequest.java
+++ src/main/java/com/Auth/DTO/SignUpRequest.java
@@ -1,2 +1,27 @@
-package com.Auth.DTO;public class SignUpRequest {
+package com.Auth.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Schema(description =  "Запрос на регистрацию")
+public class SignUpRequest {
+
+    @Schema(description = "Имя пользователя", example = "Nadar")
+    @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
+    @NotBlank(message = "имя пользователя не должно быть пустым")
+    private String username;
+
+    @Schema(description = "Адрес электронной почты", example = "donat290817@gmail.com")
+    @Size(min = 5, max = 255, message = "Адрес электронной почты должен содержать от 5 до 255")
+    @NotBlank(message = "Поле адреса электронной почты не должно быть пустым")
+    @Email(message = "Email адрес должен быть в формате user@example.com")
+    private String email;
+
+    @Schema(description = "Пароль", example = "jfsvdckzxl")
+    @Size(max = 255, message = "Длина пароль не должна превышать 255 символов")
+    private String password;
 }
